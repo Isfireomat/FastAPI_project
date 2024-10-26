e6e26a060bef5c7a3d19eade296352fca9149547
Isfireomat <dfdfgfgf666@gmail.com>
2024-10-26 20:55:30 +0300

    add html and css

--- app/api/routers/routers.py
+++ app/api/routers/routers.py
@@ -5,15 +5,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.database import get_db
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 router = APIRouter()
+# router.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-templates = Jinja2Templates(directory="api/app/templates")
+
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def login_page(request: Request):
     return {"pzdc":1}
+
+@router.get("/registration", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
 
 @router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api.routers import routers,auth_routers
+from fastapi.staticfiles import StaticFiles
+# from fastapi.middleware.cors import CORSMiddleware
 
 app:FastAPI=FastAPI(title='Tested_FastAPI_Project')
+# from fastapi.middleware.cors import CORSMiddleware
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(routers.router)
 app.include_router(auth_routers.router)
