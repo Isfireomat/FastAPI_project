e409c33c78755288c6cdc1a86873fd587d98390b
Isfireomat <dfdfgfgf666@gmail.com>
2024-10-29 12:19:32 +0300

    chore add plagiat coutput

--- app/api/routers/photo_routers.py
+++ app/api/routers/photo_routers.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.db_connect import get_session
 from app.api.db_crud import create_picture, get_picture_by_bytes, get_pictures
 from app.api.utils.picture_utils import compare_images
+from app.api import schemas
 
 router = APIRouter()
 
@@ -12,10 +13,11 @@ async def upload_photo(response: Response, photo: UploadFile = File(...),
                        session: AsyncSession = Depends(get_session)):
     content = await photo.read()
     pictures = await get_pictures(session)
+    result=0
     if pictures:
         result = max([compare_images(content, picture) for picture in pictures])
         picture = await get_picture_by_bytes(session, content)
-        if not picture: await create_picture(session, content)
+        if not picture: await create_picture(session, schemas.Picture(binary_picture=content))
     else: 
-        await create_picture(session, content)
-    return {"status": f"{result}"}
+        await create_picture(session, schemas.Picture(binary_picture=content))
+    return {"result": f"{result}"}
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.api.routers import routers,auth_routers
 from fastapi.staticfiles import StaticFiles
-
+from app.api.routers import routers, auth_routers, photo_routers
 app:FastAPI=FastAPI(title='Tested_FastAPI_Project')
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(routers.router)
 app.include_router(auth_routers.router)
+app.include_router(photo_routers.router)
