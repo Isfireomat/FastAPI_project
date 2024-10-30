4a7ed81bd87a365c48c29bb7db5c91bfa2150785
Isfireomat <dfdfgfgf666@gmail.com>
2024-10-30 21:06:11 +0300

    chore

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.routers import routers, auth_routers, photo_routers
+
 app:FastAPI=FastAPI(title='Tested_FastAPI_Project')
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
